fda7cf4b92c82ce105bd28fccf82f1f3d1b61774
kylepace <kylepace03@gmail.com>
2013-02-21 19:45:32 -0500

    Add position to socket stream for mouse movement.

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
-	console.log('there is a connection');
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function(data) {
+	socket.on('tracking mouse', function(data) {
 		console.log(data);
 	});
 });
