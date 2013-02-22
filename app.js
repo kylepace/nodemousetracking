f89304ac72c225aaa027d9f3c4b1d3cad99356ac
kylepace <kylepace03@gmail.com>
2013-02-21 19:26:29 -0500

    Added socket.io REALLY basic functionality

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express')
-	, engine = require('ejs-locals');
-
-var app = express();
+	, app = express()
+	, http = require('http')
+	, server = http.createServer(app)
+	, engine = require('ejs-locals')
+	, io = require('socket.io').listen(server);
 
 app.configure(function() {
 	app.engine('ejs', engine);
@@ -13,5 +15,13 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-app.listen(3000);
+io.sockets.on('connection', function(socket) {
+	console.log('there is a connection');
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function(data) {
+		console.log(data);
+	});
+});
+
+server.listen(3000);
 console.log('Listening on port 3000.');
